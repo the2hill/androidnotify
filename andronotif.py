3eba4fdcf2051619b19828fd94aeab266855adac
a2hill <dead2hill@gmail.com>
2017-05-03 11:29:59 -0500

    Updated comport and host

--- andronotif.py
+++ andronotif.py
@@ -6,19 +6,17 @@ app = Flask(__name__)
 
 @app.route("/enable")
 def enable():
-    ser = serial.Serial('COM1', 9600, timeout=0)
+    ser = serial.Serial('COM8', 9600, timeout=0)
     ser.write("Y")
     ser.close()
     return "enabled"
 
 @app.route("/disable")
 def disable():
-    ser = serial.Serial('COM1', 9600, timeout=0)
+    ser = serial.Serial('COM8', 9600, timeout=0)
     ser.write("N")
     ser.close()
     return "disabled"
 
 if __name__ == "__main__":
-    app.run()
-
-
+    app.run(host='0.0.0.0', port=9001, debug=False)
